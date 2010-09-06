3f92659176459a2098137353983fce7bbd57d2ab
David Luecke <daffstuff@neyeon.de>
2010-09-06 14:15:53 -0600

    New subscriptions

--- js/jquery.dform.js
+++ js/jquery.dform.js
@@ -19,18 +19,21 @@
 				_subscriptions[name] = new Array();
 			_subscriptions[name].push(fn);
 		},
+		// Clear all subscriptions for name
+		clear : function(name)
+		{
+			delete _subscriptions[name];
+		},
 		// Core elements
 		element :
 		{
 			/**
-			 * Create a new input element with given name and options as
-			 * attributes
+			 * Default builder function if no other element has been found
 			 */
-			text : function(options)
+			__default : function(options)
 			{
-				return _create("<input>", { type : "text" }, options);
+				return _create("<input>", {}, options);
 			},
-			
 			/**
 			 * Create a new form element with given options as attributes
 			 */
@@ -48,42 +51,61 @@
 				return _create("<select>", {}, options);
 			},
 
-			radio : function(options)
-			{
-			},
-
-			checkbox : function(options)
-			{
-				return _create("<input>", { type : "checkbox" }, options);
-			},
-
-			submit : function(options)
-			{
-				return _create("<input>", { type : "submit" }, options);
-			},
-
 			fieldset : function(options)
 			{
-				// TODO
+				return _create("<fieldset>", {}, options);
 			}
 		}
 	};
 	
 	(function _init()
-	{		
-		// Initialize core funtions
-		$.fb.subscribe("type", function(options) {
-			// TODO
-
+	{
+		$.fb.subscribe("elements", function(options) {
+			var scoper = $(this);
+			$.each(options, function(name, nested) {
+				var options = nested;
+				options["name"] = name;
+				$(scoper).formElement(options);
+			});
+			return $(this);
 		});
 		
-		$.fb.subscribe("elements", function(options) {
-			// TODO create subelements
+		$.fb.subscribe("value", function(options) {
+			$(this).val(options);
+		});
+		
+		$.fb.subscribe("options", function(options) {
+			if($(this).is("select"))
+			{
+				var scoper = $(this);
+				$.each(options, function(value, text) {
+					var option = $("<option>").attr("value", value).html(text);
+					$(scoper).append(option);
+				});
+			}
+		});
+		
+		$.fb.subscribe("default", function(options) {
+			if($(this).is("input") && $(this).attr("type") == "text")
+			{
+				$(this).data("default", options);
+				$(this).onClick(function() { 
+				});
+			}
+		});
+		
+		$.fb.subscribe("label", function(options) {
+			$(this).wrap($("<div>"));
+			var label = $("<label>").html(options);
+			$(this).parent().prepend(label);
 		});
 		
 		$.fb.subscribe("legend", function(options) {
-			var legend = $("<legend>").html(options);
-			return $(this).append(legend);
+			if($(this).is("fieldset"))
+			{
+				var legend = $("<legend>").html(options);
+				return $(this).prepend(legend);
+			}
 		});
 	})();
 	
@@ -98,18 +120,32 @@
 		{
 			// Find element options and subscription options
 			var ops = {};
-			var subsciberOps = {};
+			var subscriberOps = {};
+			var type = options["type"];
 			$.each(options, function(key, value) {
-				// element not subscribed
-				if(!_subscriptions[key] && key != "type")
+				if(!_subscriptions[key] && key != "type") // element not subscribed
 					ops[key] = value;
-				else // put in subscription options
-					subsciberOps[key] = value;
+				else if(key != "type") // put in subscription options
+					subscriberOps[key] = value;
 			});
-			// Get builder function for element and append to this
-			var builder = $.fb.element[options["type"]];
+			
+			// Get builder function for element and append to this or use default builder
+			var builder = $.fb.element.__default;
+			if($.fb.element[type])
+				builder = $.fb.element[type];
+			else
+				ops["type"] = type;
+			
+			// Call builder function
 			var element = builder(ops);
 			$(this).append(element);
+			
+			// Run subscription functions
+			$.each(subscriberOps, function(name, options) {
+				$.each(_subscriptions[name], function(i, sfn){
+					sfn.call(element, options); // run subscriber function with options
+				});
+			});
 			return $(this);
 		}
 	});
