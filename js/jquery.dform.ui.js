536ddda85223306365a355b8526b88175937914b
David Luecke <daffstuff@neyeon.de>
2010-09-07 14:00:47 -0600

    More documentation

---
+++ js/jquery.dform.ui.js
@@ -0,0 +1,34 @@
+(function($)
+{
+	$.dform.subscribe("type", function(options, type) {
+		// TODO add jQuery UI classes
+	});
+	
+	$.dform.subscribe("[type=datepicker]", function(options) {
+		// TODO datepicker
+	});
+	
+	$.dform.subscribe("[type=button]", function(options) {
+		// TODO jquery ui button
+	});
+	
+	$.dform.subscribe("[type=tabs]", function(options) {
+		// TODO jquery ui button
+	});
+	
+	$.dform.subscribe("[type=accordion]", function(options) {
+		// TODO forms in accordion
+	});
+	
+	$.dform.subscribe("[type=progressbar]", function(options) {
+		// TODO
+	});
+	
+	$.dform.subscribe("[type=autocomplete]", function(options) {
+		// TODO
+	});
+	
+	$.dform.subscribe("[type=slider]", function(options) {
+		// TODO 
+	});
+});
