2f591d0f31c3d7253e7aaa6a3f86aed40b361db8
David Luecke <daffstuff@neyeon.de>
2010-09-07 13:59:53 -0600

    More cool stuff

--- js/jquery.dform.js
+++ js/jquery.dform.js
@@ -2,8 +2,19 @@
 {
 	var _subscriptions = {};
 
-	$.fb =
+	$.dform =
 	{
+		// Get all only options that have no subscription
+		getOptions : function(options)
+		{
+			var ops = {};
+			$.each(options, function(name, value) {
+				// Add options only if no subscription exists
+				if(!$.dform.hasSubscription(name))
+					ops[name] = value;
+			});
+			return ops;
+		},
 		// Subscribe builder functions
 		subscribe : function(name, fn)
 		{
@@ -12,51 +23,86 @@
 			_subscriptions[name].push(fn);
 		},
 		// Clear all subscriptions for name
-		clear : function(name)
+		clearSubscription : function(name)
 		{
 			delete _subscriptions[name];
 		},
 		hasSubscription : function(name)
 		{
 			return _subscriptions[name] ? true : false;
+		},
+		runSubscription : function(name, element, options, type)
+		{
+			if($.dform.hasSubscription(name))
+			{
+				$.each(_subscriptions[name], function(i, sfn) {
+					// run subscriber function with options
+					sfn.call(element, options, type);
+				});
+			}
+		},
+		// Run element created by type builder function
+		createElement : function(options)
+		{
+			var type = options["type"];
+			var name = "[type=" +  options["type"] + "]";
+			var element = null;
+			var ops = {};
+			$.extend(ops, options);
+			delete ops["type"];
+			if(!type) throw "No element type given! Must always exist.";
+			if(_subscriptions[name])
+			{
+				// Run all builder functions called [type=<typename>]
+				$.each(_subscriptions[name], function(i, sfn) {
+					element = sfn.call(element, ops);
+				});
+			}
+			else
+				throw "Element type '" + type + "' does not exist";
+			return $(element);			
 		}
 	};
 	
 	$.fn.extend(
 	{
-		buildForm : function(options)
+		// Run a subscribed function on an element
+		runSubscription : function(name, options, type)
 		{
-
+			$.dform.runSubscription(name, $(this), options, type);
+			return $(this);
+		},
+		// Run all subscription functions with given options
+		runAll : function(options)
+		{
+			var type = options["type"];
+			var scoper = $(this);
+			$.each(options, function(name, sopts) {
+				// TODO each loop for list of dom elements
+				if(name != "type")
+					$(scoper).runSubscription(name, sopts, type);
+			});
+			// Run post processing functions
+			$(this).runSubscription("type", options, type);
+			return $(this);
 		},
 		// Main form element builder function
 		formElement : function(options)
 		{
-			// Initial element is the jQuery function element
-			var element = $(this);
-			// Run type builder function first
-			var type = options["type"];
-			var name = "[type=" +  options["type"] + "]";
-			delete options["type"];
-			if(_subscriptions[name])
-			{
-				$.each(_subscriptions[name], function(i, sfn) {
-					element = sfn.call(element, options);
-				});
-				
-				// Run subscription functions
-				$.each(options, function(name, options) {
-					if($.fb.hasSubscription(name))
-					{
-						$.each(_subscriptions[name], function(i, sfn) {
-							// run subscriber function with options
-							sfn.call(element, options, type);
-						});
-					}
-				});
-			}
-			else
-				throw "Element type '" + type + "' does not exist";
+			// Create element (run builder function for type)
+			var element = $.dform.createElement(options);
+			$(this).append($(element));
+			// Run all subscriptions
+			$(element).runAll(options);
 			return $(this);
+		},
+		buildForm : function(options)
+		{
+			if($(this).is("form"))
+			{
+				$(this).attr($.dform.getOptions(options));
+				$(this).runAll(options);
+			}
 		}
 	});
 })(jQuery);
--- js/jquery.dform.subscriptions.js
+++ js/jquery.dform.subscriptions.js
@@ -5,61 +5,79 @@
 	 * Use only options that have no subscriptions.
 	 * Append to parent if given.
 	 */
-	function _create(tag, defaults, options, parent)
+	function _create(tag, defaults, options)
 	{
-		var ops = {};
-		$.each(options, function(name, value) { 
-			if(!$.fb.hasSubscription(name))
-				ops[name] = value;
-		});
-		var element = $($(tag).attr($.extend(defaults, ops)));
-		if(parent)
-			$(parent).append(element);
-		return $(element);
+		var ops = $.dform.getOptions(options);
+		return $($(tag).attr($.extend(defaults, ops)));
 	}
 	
-	$.fb.subscribe("[type=text]", function(options) {
-		return _create("<input>", { "type" : "text" }, options, $(this));
+	// Builder functions
+	
+	$.dform.subscribe("[type=text]", function(options) {
+		return _create("<input>", { "type" : "text" }, options);
 	});
 
-	$.fb.subscribe("[type=password]", function(options) {
-		return _create("<input>", { "type" : "password" }, options, $(this));
+	$.dform.subscribe("[type=password]", function(options) {
+		return _create("<input>", { "type" : "password" }, options);
 	});
 	
-	$.fb.subscribe("[type=form]", function(options) {
-		return _create("<form>", { action : window.location, method : "post" }, options, $(this));
+	$.dform.subscribe("[type=form]", function(options) {
+		return _create("<form>", { action : window.location, method : "post" }, options);
 	});
 
-	$.fb.subscribe("[type=select]", function(options) {
-		return _create("<select>", {}, options, $(this));
+	$.dform.subscribe("[type=select]", function(options) {
+		return _create("<select>", {}, options);
 	});
 	
-	$.fb.subscribe("[type=fieldset]", function(options) {
-		return _create("<fieldset>", {}, options, $(this));
+	$.dform.subscribe("[type=fieldset]", function(options) {
+		return _create("<fieldset>", {}, options);
 	});
 
-	$.fb.subscribe("[type=textarea]", function(options) {
-		return _create("<textarea>", {}, options, $(this));
+	$.dform.subscribe("[type=textarea]", function(options) {
+		return _create("<textarea>", {}, options);
 	});
 	
-	$.fb.subscribe("[type=submit]", function(options) {
-		return _create("<input>", { "type" : "submit" }, options, $(this));
+	$.dform.subscribe("[type=submit]", function(options) {
+		return _create("<input>", { "type" : "submit" }, options);
+	});
+
+	$.dform.subscribe("[type=radio]", function(options) {
+		return _create("<input>", { "type" : "radio" }, options);
 	});
 	
-	$.fb.subscribe("elements", function(options) {
+	$.dform.subscribe("[type=checkbox]", function(options) {
+		return _create("<input>", { "type" : "checkbox" }, options);
+	});
+
+	$.dform.subscribe("[type=label]", function(options) {
+		return _create("<label>", {}, options);
+	});
+
+	$.dform.subscribe("[type=html]", function(options) {
+		return _create("<span>", {}, options);
+	});
+	
+	// Subscriber functions
+	
+	$.dform.subscribe("html", function(options, type) {
+		$(this).html(options);
+	});
+	
+	$.dform.subscribe("elements", function(options, type) {
 		var scoper = $(this);
-		$.each(options, function(name, nested) {
-			var options = nested;
-			options["name"] = name;
-			$(scoper).formElement(options);
+		$.each(options, function(index, nested) {
+			var values = nested;
+			if(typeof(index) == "string")
+				values["name"] = name;
+			$(scoper).formElement(values);
 		});
 	});
 	
-	$.fb.subscribe("value", function(options, type) {
+	$.dform.subscribe("value", function(options, type) {
 		$(this).val(options);
 	});
 	
-	$.fb.subscribe("options", function(options, type) {
+	$.dform.subscribe("options", function(options, type) {
 		if(type == "select")
 		{
 			// TODO optimize this
@@ -79,7 +97,7 @@
 		}
 	});
 	
-	$.fb.subscribe("hint", function(options, type) {
+	$.dform.subscribe("hint", function(options, type) {
 		if(type == "text")
 		{
 			var key = "hint";
@@ -96,20 +114,57 @@
 		}
 	});
 	
-	$.fb.subscribe("label", function(options, type) {
-		var label = $("<label>").html(options);
+	$.dform.subscribe("label", function(options, type) {
 		// TODO maybe derive ID from name
+		var labelops = { "type" : "label" };
+		if(typeof(options) == "string")
+			labelops["html"] = options;
+		else
+			$.extend(labelops, options);
+		
+		if($(this).attr("name") != "" && $(this).attr("id") == "")
+		{
+			var id = "dform-" + type + "-" + $(this).attr("name");
+			$(this).attr("id", id);
+		}
 		if($(this).attr("id"))
-			$(label).attr("for", $(this).attr("id"));
-		label = $(label).wrap("<p>").parent();
-		$(label).insertBefore($(this));
+			labelops["for"] = $(this).attr("id");
+		var label = $.dform.createElement(labelops);
+		if(type == "checkbox" || type == "radio")
+			$(this).parent().append($(label));
+		else
+			$(label).insertBefore($(this));
+		$(label).runAll(labelops);
 	});
 	
-	$.fb.subscribe("legend", function(options, type) {
+	$.dform.subscribe("legend", function(options, type) {
 		if(type == "fieldset")
 		{
 			var legend = $("<legend>").html(options);
 			$(this).prepend(legend);
 		}
+	});
+	
+	/*
+	$.dform.subscribe("[type=radiolist]", function(options) {
+		// TODO list of radiobuttons
+	});
+
+	$.dform.subscribe("[type=checkboxlist]", function(options) {
+		// TODO wrap in fieldset
+		var scoper = $(this);
+		$.each(options["entries"], function(index, entry) {
+			var cbops = { "type" : "checkbox" };
+			$.extend(cbops, entry, options);
+			delete cbops["entries"];
+			$(scoper).formElement(cbops);
+		});
+		return $(this);
+	});
+	*/
+	
+	$.dform.subscribe("type", function(options, type) {
+		if(type == "submit")
+			$(this).wrap("<p>");
 	});
 })(jQuery);
