7ce0c60d493b55255c6caea2176f9e587d783a64
daffl <daff@neyeon.de>
2010-10-14 10:53:20 -0600

    More documentation resources

--- build/docdata/javascript/custom.js
+++ build/docdata/javascript/custom.js
@@ -2,11 +2,27 @@
  * @author daff
  */
 $(document).ready(function() {
-	$("pre").each(function() { $(this).wrapInner("<code>"); });
 	$.beautyOfCode.init({
 		brushes: ['JScript', 'Plain'],
 		ready: function() {
 				$("pre").beautifyCode('javascript');
 			}
+	});
+	
+	$("pre").each(function() { $(this).wrapInner("<code>"); });
+	
+	$(".CSubscriber pre, .CType pre").each(function(count) { 	
+		var elem = $(this).parent();
+		var codeid = "code-" + count;
+		var formid = "form-" + count;
+		$(elem).wrap($("<div>").attr("id", codeid));
+		$(elem).parent().wrap($("<div>"));
+		var tabs = $(elem).parent().parent();
+		$(tabs).prepend($("<div>").attr("id", formid));
+		var ul = $("<ul>")
+			.append($("<li>").append($("<a>").attr("href", "#" + formid).html("Form")))
+			.append($("<li>").append($("<a>").attr("href", "#" + codeid).html("JSON")));
+		$(tabs).prepend(ul);
+		$(tabs).tabs();
 	});
 });
