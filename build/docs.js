b283a4855b09fbdce01b3779ff68d1eb51ca160f
daffl <daff@neyeon.de>
2012-03-12 18:48:46 -0600

    Added tag and removed documentation generator

--- build/docs.js
+++
@@ -1,9 +0,0 @@
-load('steal/rhino/rhino.js');
-steal.overwrite = true;
-load('documentjs/documentjs.js');
-
-
-DocumentJS('src', {
-	markdown : ['readme.md', 'changelog.md', 'license.md'],
-	out : 'docs'
-});
